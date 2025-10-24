7e62cc3d339efde41a47188a924e34ab4a20281c
xxLYNX <cullenscout1970@gmail.com>
2025-10-24 15:12:24 -0400

    Obsidian Arrow Framework - ModularArchery

---
+++ ModularModules-1.21.10/src/main/java/name/modid/ModularArchery.java
@@ -0,0 +1,36 @@
+package name.modid;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+
+import java.util.function.Function;
+
+public class ModularArchery {
+    public static Item register(String name, Function<Item.Settings, Item> itemFactory, Item.Settings settings){
+        // Create the item key.
+        RegistryKey<Item> itemKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(ModularModules.MOD_ID, name));
+
+        // Create the item instance.
+        Item item = itemFactory.apply(settings.registryKey(itemKey));
+
+        // Register the item.
+        Registry.register(Registries.ITEM, itemKey, item);
+
+        return item;
+    }
+
+    public static final Item OBSIDIAN_ARROW =
+            register("obsidian_arrow", Item::new, new Item.Settings());
+
+    public static void initialize() {
+        // Get the event for modifying entries in the ingredients group.
+        // And register an event handler that adds our suspicious item to the ingredients group.
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS)
+                .register((itemGroup) -> itemGroup.add(ModularArchery.OBSIDIAN_ARROW));
+    }
+}
