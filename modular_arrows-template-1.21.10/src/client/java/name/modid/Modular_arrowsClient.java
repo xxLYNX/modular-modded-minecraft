fb0f7380812414852a62df28c85827ba3b6cce98
xxLYNX <cullenscout1970@gmail.com>
2025-10-23 20:13:58 -0400

    Proto mod

---
+++ modular_arrows-template-1.21.10/src/client/java/name/modid/Modular_arrowsClient.java
@@ -0,0 +1,14 @@
+package name.modid;
+
+import net.fabricmc.api.ClientModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Modular_arrowsClient implements ClientModInitializer {
+
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+
+	}
+}
